649882f620e2a4e59a25bff4c8e8d34adb70e6fa
david <evil.dave59@gmail.com>
2024-01-19 17:15:26 +0100

    ajout du sass

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,10 @@
-import styles from '../styles/Home.module.css';
+import styles from "../styles/Home.module.scss";
 
 function Home() {
   return (
     <div>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>portfolio David</h1>
       </main>
     </div>
   );
